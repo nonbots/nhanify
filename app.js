934bc0ce9e4d142dceb1c965847c31da9f3a1268
nonbots <nhantdbui@gmail.com>
2024-06-14 17:15:55 -0400

    Provide pug with user info to display based on user sign in status
    Create session to store user
    Store user session on res.locals for pug to access user data
    Remove userId from constructor and userId is passed in to persistence
    methods when needed
    Display login and signout information based on if user is logged in or
    signed out

--- app.js
+++ app.js
@@ -1,9 +1,11 @@
 const express = require("express");
 const app = express();
 const session = require("express-session");
+const store = require("connect-loki");
+const LokiStore = store(session);
 const morgan = require("morgan");
 const Persistence = require("./lib/pg-persistence.js");
-const persistence = new Persistence(1);
+const persistence = new Persistence();
 const port = 3002;
 const host = "localhost";
 const { getUpdatedPlaylist } = require("./lib/playlist.js");
@@ -12,9 +14,29 @@ app.set("view engine", "pug");
 app.use(express.static("public"));
 app.use(morgan("common"));
 app.use(express.urlencoded({ extended: true }));
+app.use(
+  session({
+    cookie: {
+      httpOnly: true,
+      maxAge: 3_600_000 * 1,
+      path: "/",
+      secure: false,
+    },
+    name: "nhanify-id",
+    resave: false,
+    saveUninitialized: true,
+    secret: "this is not very secure",
+    store: new LokiStore({}),
+  }),
+);
+
+app.use((req, res, next) => {
+  res.locals.user = req.session.user;
+  next();
+});
 
 app.get("/playlists", async (req, res) => {
-  const playlists = await persistence.getPlaylists();
+  const playlists = await persistence.getPlaylists(req.session.user.id);
   res.render("playlists", { playlists });
 });
 
@@ -34,9 +56,14 @@ app.post("/login", async (req, res) => {
   const username = req.body.username;
   const password = req.body.password;
   console.log({ username });
-  const user = await persistence.validateUser(username, password);
+  const user = await persistence.validateUsername(username);
   if (user) {
+    //authenicate user
+
     //store in session
+    req.session.user = user;
+    //redired to playlists page
+    res.redirect("/playlists");
   } else {
     //display an error message to user
   }
