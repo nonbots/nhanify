76f4983578bf8af44da16d2b9a599e19df050fea
mAmineChniti <emin.chniti@esprit.tn>
2024-09-26 22:26:41 +0100

    mb chief part 2, fixed process.env stuff

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -2,7 +2,19 @@ import globals from "globals";
 import pluginJs from "@eslint/js";
 
 export default [
-  { files: ["**/*.js"], languageOptions: { sourceType: "commonjs" } },
-  { languageOptions: { globals: globals.browser } },
+  {
+    files: ["**/*.js"],
+    languageOptions: {
+      sourceType: "commonjs",
+      globals: {
+        ...globals.node,
+      },
+    },
+  },
+  {
+    languageOptions: {
+      globals: globals.browser,
+    },
+  },
   pluginJs.configs.recommended,
 ];
